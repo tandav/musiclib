8389ceaf7e0b54c672c57a376a58b008b2283a28
Alexander Rodionov <alx1dr@gmail.com>
2021-08-12 18:00:04 +0300

    fix scale render issues (same image overlay)

--- piano.py
+++ piano.py
@@ -4,8 +4,6 @@ import config
 
 font = ImageFont.truetype('res/SFNSMono.ttf', 40)
 piano_template = Image.open("res/piano_template-grey.png").convert("RGBA")
-layer = Image.new("RGBA", piano_template.size, (255,255,255,0))
-d = ImageDraw.Draw(layer)
 
 
 white_x0, white_dx, white_y = 24, 73, 351
@@ -38,28 +36,27 @@ note_xy = {
     (config.chromatic_notes[11], 1): (white_x0 + white_dx * 13, white_y),
 }
 
-
-
-def add_square(xy, text):
-    x, y = xy
-    r = 50
-    padding_x = 12
-    padding_y = 1
-    red_bigger = -2
-    red_x = x - red_bigger
-    red_y = y - red_bigger
-    red_r = r + red_bigger * 2
-
-    # d.rectangle((x - padding_x, y - padding_y, x - padding_x + r , y - padding_y + r), fill=(0,0,0,0), outline=(0,0,0), width=4)
-    # d.rectangle((red_x - padding_x, red_y - padding_y, red_x - padding_x + red_r , red_y - padding_y + red_r), fill=(255,255,255,255), outline=(255,0,0), width=5)
-    d.rectangle((red_x - padding_x, red_y - padding_y, red_x - padding_x + red_r, red_y - padding_y + red_r), fill=(255, 255, 255, 255))
-    d.text((x, y), text, font=font, fill=(0, 0, 0, 255))
-
-
 def scale_to_piano(scale):
+    layer = Image.new("RGBA", piano_template.size, (255, 255, 255, 0))
+    d = ImageDraw.Draw(layer)
+
+    def add_square(xy, text):
+        x, y = xy
+        r = 50
+        padding_x = 12
+        padding_y = 1
+        red_bigger = -2
+        red_x = x - red_bigger
+        red_y = y - red_bigger
+        red_r = r + red_bigger * 2
+
+        # d.rectangle((x - padding_x, y - padding_y, x - padding_x + r , y - padding_y + r), fill=(0,0,0,0), outline=(0,0,0), width=4)
+        # d.rectangle((red_x - padding_x, red_y - padding_y, red_x - padding_x + red_r , red_y - padding_y + red_r), fill=(255,255,255,255), outline=(255,0,0), width=5)
+        d.rectangle((red_x - padding_x, red_y - padding_y, red_x - padding_x + red_r, red_y - padding_y + red_r), fill=(255, 255, 255, 255))
+        d.text((x, y), text, font=font, fill=(0, 0, 0, 255))
+
     i = 0
     for (note, octave), xy in note_xy.items():
-        print(i, scale[i], note, note == scale[i])
         if note == scale[i]:
             add_square(xy, note)
             i += 1
@@ -68,4 +65,4 @@ def scale_to_piano(scale):
     # for octave, note in itertools.product((0, 1), config.chromatic_notes):
     #     add_square(note_xy[(note, octave)], note)
     out = Image.alpha_composite(piano_template, layer)
-    return  out
+    return out
