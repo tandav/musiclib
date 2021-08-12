751d090bb1b94ede790ac441c563ede5ac69e364
Alexander Rodionov <alx1dr@gmail.com>
2021-08-12 17:55:06 +0300

    add scale support

--- piano.py
+++ piano.py
@@ -11,31 +11,31 @@ d = ImageDraw.Draw(layer)
 white_x0, white_dx, white_y = 24, 73, 351
 black_x0, black_dx, black_y = 61, 73, 255
 
-note_loc = {
+note_xy = {
     (config.chromatic_notes[ 0], 0): (white_x0 + white_dx *  0, white_y),
+    (config.chromatic_notes[ 1], 0): (black_x0 + black_dx *  0, black_y),
     (config.chromatic_notes[ 2], 0): (white_x0 + white_dx *  1, white_y),
+    (config.chromatic_notes[ 3], 0): (black_x0 + black_dx *  1, black_y),
     (config.chromatic_notes[ 4], 0): (white_x0 + white_dx *  2, white_y),
     (config.chromatic_notes[ 5], 0): (white_x0 + white_dx *  3, white_y),
+    (config.chromatic_notes[ 6], 0): (black_x0 + black_dx *  3, black_y),
     (config.chromatic_notes[ 7], 0): (white_x0 + white_dx *  4, white_y),
+    (config.chromatic_notes[ 8], 0): (black_x0 + black_dx *  4, black_y),
     (config.chromatic_notes[ 9], 0): (white_x0 + white_dx *  5, white_y),
+    (config.chromatic_notes[10], 0): (black_x0 + black_dx *  5, black_y),
     (config.chromatic_notes[11], 0): (white_x0 + white_dx *  6, white_y),
     (config.chromatic_notes[ 0], 1): (white_x0 + white_dx *  7, white_y),
+    (config.chromatic_notes[ 1], 1): (black_x0 + black_dx *  7, black_y),
     (config.chromatic_notes[ 2], 1): (white_x0 + white_dx *  8, white_y),
+    (config.chromatic_notes[ 3], 1): (black_x0 + black_dx *  8, black_y),
     (config.chromatic_notes[ 4], 1): (white_x0 + white_dx *  9, white_y),
     (config.chromatic_notes[ 5], 1): (white_x0 + white_dx * 10, white_y),
-    (config.chromatic_notes[ 7], 1): (white_x0 + white_dx * 11, white_y),
-    (config.chromatic_notes[ 9], 1): (white_x0 + white_dx * 12, white_y),
-    (config.chromatic_notes[11], 1): (white_x0 + white_dx * 13, white_y),
-    (config.chromatic_notes[ 1], 0): (black_x0 + black_dx *  0, black_y),
-    (config.chromatic_notes[ 3], 0): (black_x0 + black_dx *  1, black_y),
-    (config.chromatic_notes[ 6], 0): (black_x0 + black_dx *  3, black_y),
-    (config.chromatic_notes[ 8], 0): (black_x0 + black_dx *  4, black_y),
-    (config.chromatic_notes[10], 0): (black_x0 + black_dx *  5, black_y),
-    (config.chromatic_notes[ 1], 1): (black_x0 + black_dx *  7, black_y),
-    (config.chromatic_notes[ 3], 1): (black_x0 + black_dx *  8, black_y),
     (config.chromatic_notes[ 6], 1): (black_x0 + black_dx * 10, black_y),
+    (config.chromatic_notes[ 7], 1): (white_x0 + white_dx * 11, white_y),
     (config.chromatic_notes[ 8], 1): (black_x0 + black_dx * 11, black_y),
+    (config.chromatic_notes[ 9], 1): (white_x0 + white_dx * 12, white_y),
     (config.chromatic_notes[10], 1): (black_x0 + black_dx * 12, black_y),
+    (config.chromatic_notes[11], 1): (white_x0 + white_dx * 13, white_y),
 }
 
 
@@ -56,8 +56,16 @@ def add_square(xy, text):
     d.text((x, y), text, font=font, fill=(0, 0, 0, 255))
 
 
-def scale_to_piano():
-    for octave, note in itertools.product((0, 1), config.chromatic_notes):
-        add_square(note_loc[(note, octave)], note)
+def scale_to_piano(scale):
+    i = 0
+    for (note, octave), xy in note_xy.items():
+        print(i, scale[i], note, note == scale[i])
+        if note == scale[i]:
+            add_square(xy, note)
+            i += 1
+            if i == len(scale):
+                break
+    # for octave, note in itertools.product((0, 1), config.chromatic_notes):
+    #     add_square(note_xy[(note, octave)], note)
     out = Image.alpha_composite(piano_template, layer)
     return  out
