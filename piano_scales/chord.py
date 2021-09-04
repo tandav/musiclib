a799e24d1bdb5df6af0c8022968d9ff599127d7d
Alexander Rodionov <alx1dr@gmail.com>
2021-09-04 12:53:26 +0300

    add link to abstract Chord from SpecificChord

--- piano_scales/chord.py
+++ piano_scales/chord.py
@@ -150,6 +150,8 @@ class SpecificChord:
     ):
         self.notes = notes
         self.root = root
+        self.abstract = Chord(frozenset(note.abstract for note in notes), root)
+
     def short_repr(self): return ' '.join(note.short_repr() for note in self.notes)
 
     # def add_specific_notes(self):
