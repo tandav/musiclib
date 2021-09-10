a6758d1c76f2ce11ca907b3f12083b8f0811821a
Alexander Rodionov <alx1dr@gmail.com>
2021-09-10 19:28:51 +0300

    change midi codes to match octaves in ableton

--- piano_scales/midi.py
+++ piano_scales/midi.py
@@ -5,7 +5,8 @@ if (midi_device := os.environ.get('MIDI_DEVICE')):
     port = mido.open_output(midi_device)
 
     def send_message(*args, **kwargs):
-        port.send(mido.Message(*args, **kwargs))
+        note = kwargs.pop('note') + 24  # to match ableton octaves
+        port.send(mido.Message(*args, note=note, **kwargs))
 else:
     def send_message(*args, **kwargs):
         print(*args, ', '.join(f'{k}={v!r}' for k, v in kwargs.items()))
