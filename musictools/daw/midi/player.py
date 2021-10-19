be587c7f9cca348c7f032cc440b56dabccca5f3c
Alexander Rodionov <alx1dr@gmail.com>
2021-10-20 00:58:55 +0300

    refactor rhythm_to_midi function

--- musictools/daw/midi/player.py
+++ musictools/daw/midi/player.py
@@ -1,18 +1,26 @@
 import os
+import mido
+
 from typing import Optional
 
+
 if (midi_device := os.environ.get('MIDI_DEVICE')):
-    import mido
-
-
     port = mido.open_output(midi_device)
-
     def send_message(*args, **kwargs):
         note = kwargs.pop('note') + 24  # to match ableton octaves
         port.send(mido.Message(*args, note=note, **kwargs))
 
-    def rhythm_to_midi(rhythm, path, chord=None, progression=None, return_midi=False) -> Optional[mido.MidiFile]:
+
+else:
+    port = None
+
+    def send_message(*args, **kwargs):
+        print(*args, ', '.join(f'{k}={v!r}' for k, v in kwargs.items()))
+
+
+    def rhythm_to_midi(rhythm, path=None, note_=None, chord=None, progression=None) -> Optional[mido.MidiFile]:
         mid = mido.MidiFile(type=0, ticks_per_beat=96)
+
         ticks_per_note = mid.ticks_per_beat * rhythm.beats_per_bar // rhythm.bar_notes
         track = mido.MidiTrack()
         track.append(mido.MetaMessage(type='track_name', name='test_name'))
@@ -23,7 +31,7 @@ if (midi_device := os.environ.get('MIDI_DEVICE')):
             nonlocal t
             for is_play in rhythm.notes:
                 if is_play:
-                    notes = [48] if chord is None else [note.absolute_i for note in chord.notes]
+                    notes = [note_.absolute_i] if chord is None else [note.absolute_i for note in chord.notes]
                     for i, note in enumerate(notes):
                         track.append(mido.Message('note_on', note=note, velocity=100, time=t if i == 0 else 0))
                     for i, note in enumerate(notes):
@@ -39,9 +47,6 @@ if (midi_device := os.environ.get('MIDI_DEVICE')):
                 append_bar(chord)
 
         mid.tracks.append(track)
-        if return_midi:
+        if path is None:
             return mid
         mid.save(path)
-else:
-    def send_message(*args, **kwargs):
-        print(*args, ', '.join(f'{k}={v!r}' for k, v in kwargs.items()))
