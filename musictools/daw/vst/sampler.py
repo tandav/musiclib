d6cf73ae7556f114d72a90ff2c80c442b2cb5801
Alexander Rodionov <alx1dr@gmail.com>
2021-11-02 19:20:46 +0300

    clap decay improvements

--- musictools/daw/vst/sampler.py
+++ musictools/daw/vst/sampler.py
@@ -22,7 +22,7 @@ class Sampler(VST):
 
     DEFAULT_NOTE_TO_ADSR = {
         SpecificNote('C', 3): ADSR(attack=0.001, decay=0.1, sustain=1, release=0.6),
-        SpecificNote('e', 3): ADSR(attack=0.001, decay=0.15, sustain=0, release=0.1),
+        SpecificNote('e', 3): ADSR(attack=0.001, decay=0.2, sustain=0, release=0.1),
         SpecificNote('b', 3): ADSR(attack=0.001, decay=0.15, sustain=0, release=0.1),
     }
 
