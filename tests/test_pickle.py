c73a92b283ce1a61a9a166b04b7a56db475d6c6b
Alexander Rodionov <alx1dr@gmail.com>
2021-11-09 18:07:50 +0300

    test note, chord, scale are picklable

---
+++ tests/test_pickle.py
@@ -0,0 +1,28 @@
+import pickle
+import pytest
+
+from musictools.note import Note
+from musictools.note import SpecificNote
+from musictools.chord import Chord
+from musictools.chord import SpecificChord
+from musictools.scale import Scale
+
+
+@pytest.mark.parametrize('note', (Note('C'), SpecificNote('C', 3)))
+def test_note(note):
+    pickle.dumps(note)
+
+
+@pytest.mark.parametrize('chord', (
+    Chord(frozenset({Note('C'), Note('E'), Note('G')}), root=Note('C')),
+    SpecificChord(frozenset({SpecificNote('C', 5), SpecificNote('E', 5), SpecificNote('G', 5)})),
+))
+def test_chord(chord):
+    pickle.dumps(chord)
+
+
+@pytest.mark.parametrize('scale', (
+    Scale('C', 'major'),
+))
+def test_scale(scale):
+    pickle.dumps(scale)
