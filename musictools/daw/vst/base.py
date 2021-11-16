4466463b6e21ccd60af2df1a4c31e6e36468da07
Alexander Rodionov <alx1dr@gmail.com>
2021-11-16 20:24:36 +0300

    transpose feature for any VST

--- musictools/daw/vst/base.py
+++ musictools/daw/vst/base.py
@@ -9,15 +9,16 @@ from musictools.note import SpecificNote
 
 
 class VST(abc.ABC):
-    def __init__(self, adsr: Union[ADSR, dict[SpecificNote, ADSR]] = ADSR(), amplitude=0.1):
+    def __init__(self, adsr: Union[ADSR, dict[SpecificNote, ADSR]] = ADSR(), amplitude=0.1, transpose: int = 0):
         self._adsr = adsr
         self.amplitude = amplitude
         self.mute = False
+        self.transpose = transpose
 
     def adsr(self, note): return self._adsr
 
     def note_to_freq(self, note: SpecificNote):
-        return (config.tuning / 32) * (2 ** ((note.absolute_i - 9) / 12))
+        return (config.tuning / 32) * (2 ** ((note.absolute_i + self.transpose - 9) / 12))
 
     def samples_to_t(self, ns_rendered: int, ns_to_render: int):
         t0 = ns_rendered / config.sample_rate
