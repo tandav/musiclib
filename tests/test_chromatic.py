d36115d51cd23b435f426d3b0eb94cd3830de960
Alexander Rodionov <alx1dr@gmail.com>
2022-01-03 20:09:13 +0300

    fix failing test_chromatic.test_sort (use tuple instead of list)

--- tests/test_chromatic.py
+++ tests/test_chromatic.py
@@ -20,8 +20,8 @@ def test_sort():
     assert ''.join(chromatic.sort_notes('bAGDCfFedBaE')) == 'CdDeEFfGaAbB'
     assert ''.join(chromatic.sort_notes(set('bAGDCfFedBaE'))) == 'CdDeEFfGaAbB'
     assert ''.join(chromatic.sort_notes('BCfGFFBDDGfbAEBeeEEf')) == 'CDDeeEEEFFfffGGAbBBB'
-    assert chromatic.sort_notes((Note('A'), Note('C'), Note('b'))) == [Note('C'), Note('A'), Note('b')]
-    assert chromatic.sort_notes((SpecificNote('A', 5), SpecificNote('C', 2), SpecificNote('b', 9))) == [SpecificNote('C', octave=2), SpecificNote('A', 5), SpecificNote('b', 9)]
+    assert chromatic.sort_notes((Note('A'), Note('C'), Note('b'))) == (Note('C'), Note('A'), Note('b'))
+    assert chromatic.sort_notes((SpecificNote('A', 5), SpecificNote('C', 2), SpecificNote('b', 9))) == (SpecificNote('C', octave=2), SpecificNote('A', 5), SpecificNote('b', 9))
 
     # with start note
     assert ''.join(chromatic.sort_notes('CDEFGAB', start='G')) == 'GABCDEF'
