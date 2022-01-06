82dcddc50f117c1886634e516b4f561027ba5094
Alexander Rodionov <alx1dr@gmail.com>
2022-01-06 22:08:17 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
+from setuptools import find_packages
 from setuptools import setup
+
 
 setup(
     name='musictool',
-    version='1.0.2',
+    version='1.0.3',
     description='set of tools to help learning scales, modes, modulations, chord progressions, voice leading, rhythm',
     long_description_content_type="text/markdown",
     url='https://github.com/tandav/musictool',
-    packages=['musictool'],
+    packages=['musictool', 'musictool.*'],
 )
