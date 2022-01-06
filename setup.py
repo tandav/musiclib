966ce80413798361f0eec34312c4f42b8c2d07cf
Alexander Rodionov <alx1dr@gmail.com>
2022-01-06 23:48:49 +0300

    add mido dependency to install_requires in setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,12 @@ from setuptools import setup
 
 setup(
     name='musictool',
-    version='1.1.0',
+    version='1.1.1',
     description='set of tools to help learning scales, modes, modulations, chord progressions, voice leading, rhythm',
     long_description_content_type="text/markdown",
     url='https://github.com/tandav/musictool',
     packages=find_namespace_packages(exclude=('static*', 'tests*')),
+    install_requires=[
+        'mido',
+    ],
 )
