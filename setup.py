a427ee47d4b29a38e3b6cd0dc9003855fe1036d1
Alexander Rodionov <alx1dr@gmail.com>
2022-01-07 00:47:42 +0300

    add pipe21 req

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from setuptools import setup
 
 setup(
     name='musictool',
-    version='1.1.3',
+    version='1.1.4',
     description='set of tools to help learning scales, modes, modulations, chord progressions, voice leading, rhythm',
     long_description_content_type="text/markdown",
     url='https://github.com/tandav/musictool',
     packages=find_namespace_packages(exclude=('static*', 'tests*')),
     install_requires=[
         'mido',
+        'pipe21',
     ],
 )
