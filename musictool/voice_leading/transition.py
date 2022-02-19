dc786d7e3d33edfc88b799abf379cac7322f7f4c
Alexander Rodionov <tandav@tandav.me>
2022-02-19 16:52:30 +0300

    cleanup progression.py, move some code to transition.py

--- musictool/voice_leading/transition.py
+++ musictool/voice_leading/transition.py
@@ -1,5 +1,8 @@
+import collections
+
 from musictool.chord import SpecificChord
 from musictool.note import SpecificNote
+from musictool.noteset import NoteRange
 
 
 class Transition:
@@ -19,3 +22,39 @@ class Transition:
             '  '.join(self.arrow(na, nb) for na, nb in zip(self.a, self.b)),
             str(self.b),
         ))
+
+
+def chord_transitons(
+    chord: SpecificChord,
+    noterange: NoteRange,
+    unique_abstract: bool = False,
+) -> frozenset[SpecificChord]:
+    out = set()
+    for note in chord:
+        for add in (-1, 1):
+            if (new_note := noterange.noteset.add_note(note, add)) not in noterange:
+                continue
+            notes = chord.notes - {note} | {new_note}
+            if len(notes) != len(chord.notes):
+                continue
+            if unique_abstract and len(notes) > len({n.abstract for n in notes}):
+                continue
+            out.add(SpecificChord(notes))
+    return frozenset(out)
+
+
+def transition_graph(start_chord: SpecificChord, noterange: NoteRange) -> dict[SpecificChord, frozenset[SpecificChord]]:
+    graph = collections.defaultdict(set)
+
+    def _graph(chord: SpecificChord):
+        if chord in graph:
+            return
+        childs = chord_transitons(chord, noterange)
+        for child in childs:
+            graph[chord].add(child)
+        for child in childs:
+            _graph(child)
+
+    _graph(start_chord)
+    graph = dict(graph)
+    return graph
--- tests/voice_leading/test_transition.py
+++ tests/voice_leading/test_transition.py
@@ -3,7 +3,11 @@ import textwrap
 import pytest
 
 from musictool.chord import SpecificChord
-from musictool.voice_leading.transition import Transition
+from musictool.note import SpecificNote
+from musictool.noteset import NoteRange
+from musictool.noteset import NoteSet
+from musictool.scale import Scale
+from musictool.voice_leading import transition
 
 
 @pytest.mark.parametrize('a, b, expected', (
@@ -17,4 +21,35 @@ from musictool.voice_leading.transition import Transition
     ),
 ))
 def test_transition(a, b, expected):
-    assert repr(Transition(SpecificChord.from_str(a), SpecificChord.from_str(b))) == textwrap.dedent(expected)
+    assert repr(transition.Transition(SpecificChord.from_str(a), SpecificChord.from_str(b))) == textwrap.dedent(expected)
+
+
+@pytest.mark.parametrize('noterange, noteset, chord_str, transitions, unique_abstract', (
+    (('A0', 'D2'), Scale.from_name('C', 'major'), 'C1_E1_G1', {'B0_E1_G1', 'C1_D1_G1', 'C1_E1_A1', 'C1_E1_F1', 'C1_F1_G1', 'D1_E1_G1'}, False),
+    (('A0', 'D2'), NoteSet(frozenset('CDEFGAB')), 'C1_E1_G1', {'B0_E1_G1', 'C1_D1_G1', 'C1_E1_A1', 'C1_E1_F1', 'C1_F1_G1', 'D1_E1_G1'}, False),
+    (('A0', 'D2'), NoteSet(frozenset('CDEFGAB')), 'C1_D1_E1', {'B0_D1_E1', 'C1_D1_F1'}, False),
+    (('A0', 'D2'), NoteSet(frozenset('CDEFGAB')), 'B0_C1_D1', {'A0_C1_D1', 'B0_C1_E1'}, False),
+    (('A0', 'D2'), NoteSet(frozenset('CDEFGAB')), 'B1_C2_D2', {'A1_C2_D2'}, False),
+    (('A0', 'D2'), NoteSet(frozenset('CDEFGAB')), 'A1_B1_C2', {'G1_B1_C2', 'A1_B1_D2'}, False),
+    (('A0', 'D2'), NoteSet(frozenset('CDEFGAB')), 'F1_G1_A1', {'E1_G1_A1', 'F1_G1_B1'}, False),
+    (('A0', 'D2'), NoteSet(frozenset('CDEFGAB')), 'D1_E1_C2', {'C1_E1_C2', 'D1_E1_B1', 'D1_E1_D2', 'D1_F1_C2'}, False),
+    (('A0', 'D2'), NoteSet(frozenset('CDEFGAB')), 'D1_E1_C2', {'D1_E1_B1', 'D1_F1_C2'}, True),
+    (('A0', 'D2'), NoteSet(frozenset('CDEFGAB')), 'C1_E1', {'B0_E1', 'C1_D1', 'C1_F1', 'D1_E1'}, False),
+    (('A0', 'D2'), NoteSet(frozenset('CDEFGAB')), 'C1_D1', {'B0_D1', 'C1_E1'}, False),
+    (('A0', 'D2'), NoteSet(frozenset('CDEFGAB')), 'C1_D1_E1_F1', {'B0_D1_E1_F1', 'C1_D1_E1_G1'}, False),
+    (('A0', 'D2'), NoteSet(frozenset('CDEFGAB')), 'C1_D1_E1_F1', {'B0_D1_E1_F1', 'C1_D1_E1_G1'}, False),
+    (('A0', 'D2'), NoteSet(frozenset('CDEFGAB')), 'C1_E1_G1_B1', {'B0_E1_G1_B1', 'D1_E1_G1_B1', 'C1_E1_F1_B1', 'C1_E1_A1_B1', 'C1_E1_G1_C2', 'C1_F1_G1_B1', 'C1_D1_G1_B1', 'C1_E1_G1_A1'}, False),
+    (('A0', 'D2'), NoteSet(frozenset('CDEFGAB')), 'C1_E1_G1_B1', {'D1_E1_G1_B1', 'C1_D1_G1_B1', 'C1_E1_F1_B1', 'C1_E1_A1_B1', 'C1_E1_G1_A1', 'C1_F1_G1_B1'}, True),
+    (('a0', 'd2'), NoteSet(frozenset('CdeFGab')), 'C1_e1_G1', {'b0_e1_G1', 'C1_d1_G1', 'C1_e1_a1', 'C1_e1_F1', 'C1_F1_G1', 'd1_e1_G1'}, False),
+))
+def test_chord_transitons(noterange, noteset, chord_str, transitions, unique_abstract):
+    chord = SpecificChord.from_str(chord_str)
+    noterange = NoteRange(SpecificNote.from_str(noterange[0]), SpecificNote.from_str(noterange[1]), noteset)
+    assert set(map(str, transition.chord_transitons(chord, noterange, unique_abstract))) == transitions
+
+
+def test_transition_graph():
+    noterange = NoteRange(SpecificNote('A', 0), SpecificNote('D', 2), NoteSet(frozenset('CDEFGAB')))
+    graph = transition.transition_graph(SpecificChord.from_str('C1_E1_G1'), noterange)
+    assert len(graph) == 165
+    assert sum(map(len, graph.values())) == 720
