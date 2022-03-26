c9d7c6669b1896c1f6cc18356eea00603ec11eb3
Alexander Rodionov <tandav@tandav.me>
2022-03-26 09:55:48 +0300

    pin versions

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name='musictool',
-    version='1.1.31',
+    version='1.1.32',
     description='set of tools to help learning scales, modes, modulations, chord progressions, voice leading, rhythm',
     long_description_content_type="text/markdown",
     url='https://github.com/tandav/musictool',
     packages=find_namespace_packages(exclude=('static*', 'tests*')),
     install_requires=[
-        'mido',
-        'pipe21',
-        'tqdm',
+        'mido==1.2.10',
+        'pipe21==1.0.9',
+        'tqdm==4.63.0',
     ],
 )
