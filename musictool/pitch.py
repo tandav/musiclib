5585e11eff584c2d986d148bb76f2b77ff41ccb1
Alexander Rodionov <tandav@tandav.me>
2022-07-29 22:35:16 +0300

    add Pitch class for different hz/px/i operatiions

---
+++ musictool/pitch.py
@@ -0,0 +1,24 @@
+import math
+
+
+class Pitch:
+    def __init__(self, hz_tuning: float = 440):
+        self.hz_tuning = hz_tuning
+
+    def i_to_hz(self, i: float) -> float:
+        return self.hz_tuning * 2 ** (i / 12)
+
+    def hz_to_i(self, hz: float, to_int: bool = False) -> float:
+        v = 12 * math.log2(hz / self.hz_tuning)
+        return int(v) if to_int else v
+
+    @staticmethod
+    def hz_to_px(hz: float, hz_min: float, hz_max: float, px_max: float) -> float:
+        """Convert Hz to pixel position (using logarithmic scale)"""
+        return math.log2(hz / hz_min) / math.log2(hz_max / hz_min) * px_max
+
+    @staticmethod
+    def px_to_hz(px: float, hz_min: float, hz_max: float, px_max: float) -> float:
+        """Convert pixel position to hz (assuming pixel using logarithmic scale)"""
+        c = px / px_max
+        return hz_min ** (1 - c) * hz_max ** c  # type: ignore
---
+++ tests/test_pitch.py
@@ -0,0 +1,33 @@
+import pytest
+
+from musictool.pitch import Pitch
+
+
+@pytest.mark.parametrize(
+    'i, hz', [
+        (-24, 110),
+        (-12, 220),
+        (0, 440),
+        (1, 466.1637615180899),
+        (2, 493.8833012561241),
+        (12, 880),
+    ],
+)
+def test_i_hz(i, hz):
+    pitch = Pitch(hz_tuning=440)
+    assert pitch.i_to_hz(i) == hz
+    assert pitch.hz_to_i(hz, to_int=True) == i
+
+
+@pytest.mark.parametrize(
+    'hz, px, hz_min, hz_max, px_max', [
+        (55, 0, 55, 880, 1000),
+        (110, 250, 55, 880, 1000),
+        (220, 500, 55, 880, 1000),
+        (440, 750, 55, 880, 1000),
+        (880, 1000, 55, 880, 1000),
+    ],
+)
+def test_hz_to_px(hz, px, hz_min, hz_max, px_max):
+    assert Pitch.hz_to_px(hz, hz_min, hz_max, px_max) == pytest.approx(px)
+    assert Pitch.px_to_hz(px, hz_min, hz_max, px_max) == pytest.approx(hz)
