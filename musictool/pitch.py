e7e34d886e8b837fce3f4e35a15b6e82ea0fe141
Alexander Rodionov <tandav@tandav.me>
2022-08-28 14:31:50 +0300

    add Pitch.note_to_hz

--- musictool/pitch.py
+++ musictool/pitch.py
@@ -1,15 +1,22 @@
 import math
 
+from musictool.note import SpecificNote
+
 
 class Pitch:
-    def __init__(self, hz_tuning: float = 440):
+    def __init__(self, hz_tuning: float = 440, origin_note: SpecificNote = SpecificNote('A', 5)):
         self.hz_tuning = hz_tuning
+        self.origin_note = origin_note
 
     def i_to_hz(self, i: float) -> float:
         return self.hz_tuning * 2 ** (i / 12)
 
     def hz_to_i(self, hz: float) -> float:
         return 12 * math.log2(hz / self.hz_tuning)
+
+    def note_to_hz(self, note: SpecificNote) -> float:
+        print(note.i - self.origin_note.i, note - self.origin_note)
+        return self.i_to_hz(note.i - self.origin_note.i)
 
     @staticmethod
     def hz_to_px(hz: float, hz_min: float, hz_max: float, px_max: float) -> float:
--- tests/test_pitch.py
+++ tests/test_pitch.py
@@ -1,22 +1,59 @@
 import pytest
 
+from musictool.note import SpecificNote
 from musictool.pitch import Pitch
+
+A4 = SpecificNote('A', 4)
+A5 = SpecificNote('A', 5)
+C5 = SpecificNote('C', 5)
+HZ_220 = 220
+HZ_440 = 440
+HZ_493 = 493.8833012561241
+HZ_261 = 261.6255653005986
 
 
 @pytest.mark.parametrize(
     'i, hz', [
         (-24, 110),
-        (-12, 220),
+        (-12, HZ_220),
+        (-9, HZ_261),
         (0, 440),
         (1, 466.1637615180899),
-        (2, 493.8833012561241),
+        (2, HZ_493),
         (12, 880),
     ],
 )
 def test_i_hz(i, hz):
-    pitch = Pitch(hz_tuning=440)
+    pitch = Pitch(hz_tuning=HZ_440)
     assert pitch.i_to_hz(i) == hz
     assert int(pitch.hz_to_i(hz)) == i
+
+
+@pytest.mark.parametrize(
+    'hz_tuning, origin_note, i, hz', [
+        (HZ_220, A5, 0, HZ_220),
+        (HZ_220, A5, 12, HZ_440),
+        (HZ_440, A4, 0, HZ_440),
+        (HZ_440, A4, -12, HZ_220),
+    ],
+)
+def test_tuning_origin_note(hz_tuning, origin_note, i, hz):
+    pitch = Pitch(hz_tuning, origin_note)
+    assert pitch.i_to_hz(i) == hz
+    assert int(pitch.hz_to_i(hz)) == i
+
+
+@pytest.mark.parametrize(
+    'note, hz', [
+        (A4, HZ_220),
+        (A5, 440),
+        (C5, HZ_261),
+        (SpecificNote('B', 5), HZ_493),
+        (SpecificNote('A', 6), 880),
+    ],
+)
+def test_note_to_hz(note, hz):
+    assert Pitch(HZ_440).note_to_hz(note) == hz
 
 
 @pytest.mark.parametrize(
