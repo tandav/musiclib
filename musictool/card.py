11d40764c2d43825f2e333388f7ae802643d98b7
Alexander Rodionov <tandav@tandav.me>
2022-10-15 13:26:30 +0300

    fix font-weight for title

--- musictool/card.py
+++ musictool/card.py
@@ -12,7 +12,7 @@ class Card(abc.ABC):
     ) -> str:
         out = ''
         if title is not None:
-            out += f"<div style='font-size: 1em' class='card_title'>{title}</div>\n"
+            out += f"<div style='font-size: 1em; font-weight: bold' class='card_title'>{title}</div>\n"
         if subtitle is not None:
             out += f"<div style='margin-top: -0.2em;font-size: 0.8em' class='card_subtitle'>{subtitle}</div>\n"
         out = f'''
