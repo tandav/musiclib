6cd23ef51a15c55203d8b505a0b77a0862e1c650
Alexander Rodionov <tandav@tandav.me>
2022-12-11 23:32:11 +0300

    WIP

--- src/musiclib/noterange.py
+++ src/musiclib/noterange.py
@@ -13,6 +13,8 @@ CHROMATIC_NOTESET = NoteSet.from_str(config.chromatic_notes)
 
 
 class NoteRange(Sequence[SpecificNote], Card):
+    """both ends included"""
+
     def __init__(
         self,
         start: SpecificNote | str,
@@ -24,7 +26,6 @@ class NoteRange(Sequence[SpecificNote], Card):
         if isinstance(stop, str):
             stop = SpecificNote.from_str(stop)
 
-        """both ends included"""
         if start > stop:
             raise ValueError('start should be less than stop')
 
@@ -40,10 +41,9 @@ class NoteRange(Sequence[SpecificNote], Card):
         if 0 <= item < len(self):
             q = self.noteset.add_note(self.start, item)
             return q
-        elif -len(self) <= item < 0:
+        if -len(self) <= item < 0:
             return self.noteset.add_note(self.stop, item + 1)
-        else:
-            raise IndexError('index out of range')
+        raise IndexError('index out of range')
 
     @overload
     def __getitem__(self, i: int) -> SpecificNote:
@@ -56,15 +56,14 @@ class NoteRange(Sequence[SpecificNote], Card):
     def __getitem__(self, item: int | slice) -> SpecificNote | NoteRange:
         if isinstance(item, int):
             return self._getitem_int(item)
-        elif isinstance(item, slice):
+        if isinstance(item, slice):
             # if item.start is None:
             start = 0 if item.start is None else item.start
             stop = len(self) - 1 if item.stop is None else item.stop
             if not 0 <= start <= stop <= len(self):
                 raise IndexError('NoteRange slice is out of range, negative indexing is not supported')
             return NoteRange(self._getitem_int(start), self._getitem_int(stop), self.noteset)
-        else:
-            raise TypeError(f'NoteRange indices must be integers or slices, got {type(item)}')
+        raise TypeError(f'NoteRange indices must be integers or slices, got {type(item)}')
 
     def __contains__(self, item: object) -> bool:
         if not isinstance(item, SpecificNote):
