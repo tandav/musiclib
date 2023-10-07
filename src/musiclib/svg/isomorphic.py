afd17782be294ab032b15aae402e8627bdb38290
Alexander Rodionov <tandav@tandav.me>
2023-10-07 13:58:53 +0400

    wip

--- src/musiclib/svg/isomorphic.py
+++ src/musiclib/svg/isomorphic.py
@@ -16,6 +16,7 @@ from musiclib import config
 from musiclib.note import SpecificNote
 from musiclib.note import Note
 from musiclib.pitch import Pitch
+import numpy as np
 
 C4 = SpecificNote('C', 4)
 
@@ -23,29 +24,27 @@ C4 = SpecificNote('C', 4)
 class IsomorphicKeyboard(abc.ABC):
     def __init__(
         self,
-        note_colors: dict[Note | SpecificNote, Color] | None = None,
-        origin_note: SpecificNote = C4,
+        interval_colors: dict[int, Color] | None = None,
         n_rows: int = 7,
         n_cols: int = 13,
         radius: int = 30,
         font_size_radius_ratio: float = 0.5,
         round_points: bool = True,
-        note_parts_colors: dict[Note, dict[int, Color]] | None = None,
+        interval_parts_colors: dict[int, dict[int, Color]] | None = None,
     ) -> None:
         self.n_rows = n_rows
         self.n_cols = n_cols
         self.radius = radius
         self.elements: list[svg.Element] = []
-        self.note_colors = note_colors or {}
-        self.origin_note = origin_note
-        self.pitch = Pitch(origin_note=origin_note)
+        self.interval_colors = interval_colors or {}
+        # self.pitch = Pitch(origin_note=origin_note)
         self.font_size = int(radius * font_size_radius_ratio)
         self.round_points = round_points
-        self.note_parts_colors = note_parts_colors or {}
+        self.interval_parts_colors = interval_parts_colors or {}
 
         for row in range(-1, n_rows + 1):
             for col in range(-2, n_cols + 1, 2):
-                self.add_note(row, col + row % 2)
+                self.add_key(row, col + row % 2)
 
     @abc.abstractmethod
     def col_to_x(self, col: float) -> float:
@@ -65,12 +64,13 @@ class IsomorphicKeyboard(abc.ABC):
     def height(self) -> int:
         ...
 
-    def add_note(self, row: float, col: float) -> None:
-        note = self.pitch.i_to_note(col)
+    def add_key(self, row: float, col: float) -> None:
+        interval = round(col)
         x = self.col_to_x(col)
         y = self.row_to_y(row)
-        color = self.note_colors.get(note, self.note_colors.get(note.abstract, config.BLACK_PALE))
-        points = self.note_points(x, y, self.radius)
+        color = self.interval_colors.get(interval, config.BLACK_PALE)
+        points = self.key_points(x, y, self.radius)
+        print(len(points))
         if self.round_points:
             points = [round(p, 1) for p in points]
 
@@ -88,7 +88,7 @@ class IsomorphicKeyboard(abc.ABC):
         text = svg.Text(
             x=x,
             y=y,
-            text=f'{note - self.origin_note:X}',
+            text=f'{np.base_repr(interval, base=12)}',
             font_size=self.font_size,
             font_family='monospace',
             text_anchor='middle',
@@ -98,8 +98,8 @@ class IsomorphicKeyboard(abc.ABC):
             # onmousedown=f"midi_message('note_on', '{note}')",
             # onmouseup=f"midi_message('note_off', '{note}')",
         )
-        text.note = str(note)
-        text.note_midi = note.i
+        # text.note = str(note)
+        # text.note_midi = note.i
         self.elements.append(text)
 
         # transparent polygon on top for mouse events
@@ -112,26 +112,25 @@ class IsomorphicKeyboard(abc.ABC):
             # onmousedown=f"midi_message('note_on', '{note}')",
             # onmouseup=f"midi_message('note_off', '{note}')",
         )
-        polygon.note = str(note)
-        polygon.note_midi = note.i
+        # polygon.note = str(note)
+        # polygon.note_midi = note.i
         self.elements.append(polygon)
 
-        if self.note_parts_colors:
-            for part, color in self.note_parts_colors.get(note.abstract, {}).items():
-                self.elements.append(
-                    svg.Polygon(
-                        points=self.note_part_points(x, y, part),
-                        fill=Color.random().css_hex,
-                    ),
-                )
+        for part, color in self.interval_parts_colors.get(interval, {}).items():
+            self.elements.append(
+                svg.Polygon(
+                    points=self.key_part_points(x, y, part),
+                    fill=Color.random().css_hex,
+                ),
+            )
 
 
     @abc.abstractmethod
-    def note_points(self, x: float, y: float) -> list[float]:
+    def key_points(self, x: float, y: float) -> list[float]:
         ...
 
     @abc.abstractmethod
-    def note_part_points(self, x: float, y: float, part: int) -> list[float]:
+    def key_part_points(self, x: float, y: float, part: int) -> list[float]:
         ...
 
     @property
@@ -172,13 +171,13 @@ class Square(IsomorphicKeyboard):
         p = complex(y, x) + radius * cmath.exp(1j * theta)
         return p.imag, p.real
 
-    def note_points(self, x: float, y: float, radius: float) -> list[float]:
+    def key_points(self, x: float, y: float, radius: float) -> list[float]:
         points = []
         for i in range(4):
             points += self.vertex(x, y, radius, i)
         return points
     
-    def note_part_points(self, x: float, y: float, part: int) -> list[float]:
+    def key_part_points(self, x: float, y: float, part: int) -> list[float]:
         i = part // 2
         return [
             x, 
@@ -214,13 +213,13 @@ class Hex(IsomorphicKeyboard):
         p = complex(y, x) + radius * cmath.exp(1j * theta)
         return p.imag, p.real
 
-    def note_points(self, x: float, y: float, radius: float) -> list[float]:
+    def key_points(self, x: float, y: float, radius: float) -> list[float]:
         points = []
         for i in range(7):
             points += self.vertex(x, y, radius, i)
         return points
     
-    def note_part_points(self, x: float, y: float, part: int) -> list[float]:
+    def key_part_points(self, x: float, y: float, part: int) -> list[float]:
         i = part // 2
         return [
             x, 
@@ -229,7 +228,8 @@ class Hex(IsomorphicKeyboard):
             *self.vertex(x, y, self.radius, i + part % 2),
         ]
 
-class PianoRoll1D(IsomorphicKeyboard):
+
+class Piano(IsomorphicKeyboard):
     def col_to_x(self, col: float) -> float:
         return self.radius * (col + 1)
 
@@ -245,7 +245,7 @@ class PianoRoll1D(IsomorphicKeyboard):
         return int(self.row_to_y(self.n_rows))
 
     @staticmethod
-    def note_points(x: float, y: float, radius: float) -> list[float]:
+    def key_points(x: float, y: float, radius: float) -> list[float]:
         points = []
         for i in range(4):
             theta = math.pi * i / 2
