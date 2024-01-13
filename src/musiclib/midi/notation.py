9620e61390b5dd4587693c5fb94ce4b10612a9ad
Alexander Rodionov <tandav@tandav.me>
2024-01-13 12:54:15 +0300

    wip

--- src/musiclib/midi/notation.py
+++ src/musiclib/midi/notation.py
@@ -1,49 +1,35 @@
+import abc
 from musiclib.note import SpecificNote
 from musiclib.intervalset import IntervalSet
 
 
-class Header:
+class Event(abc.ABC):
     def __init__(self, code: str):
-        for line in code.strip().splitlines():
-            k, v = line.split()
-            if k == 'version':
-                self.version = v
-            elif k == 'root':
-                self.root = SpecificNote.from_str(v)
-            elif k == 'intervalset':
-                self.intervalset = IntervalSet.from_name(v)
+        self.type, *kw = code.splitlines()
+        kw = dict(kv.split(maxsplit=1) for kv in kw)
+        self.post_init(**kw)
+
+    def post_init(self, **kw):
+        pass
+
+
+class Header(Event):
+    def post_init(self, version: str, root: str, intervalset: str):
+        self.version = version
+        self.root = SpecificNote.from_str(root)
+        self.intervalset = IntervalSet.from_name(intervalset)
                     
 
-class Modulation:
-    def __init__(self, code: str):
-        _, *kvs = code.splitlines()
-        for kv in kvs:
-            k, v = kv.split(maxsplit=1)
-            if k == 'root':
-                self.root = SpecificNote.from_str(v)
-            elif k == 'intervalset':
-                self.intervalset = IntervalSet.from_name(v)
+class Modulation(Event):
+    def post_init(self, root: str, intervalset: str):
+        self.root = SpecificNote.from_str(root)
+        self.intervalset = IntervalSet.from_name(intervalset)
                         
-class Voice:
-    def __init__(self, code: str, n_intervals: int = 16):
-        i = -n_intervals * 4
-        self.name = code[:i].strip()
-        intervals_str = code[i:]
-        self.intervals = self.parse_intervals(intervals_str)
 
-    def parse_intervals(self, intervals_str: str) -> list[int | None]:
-        div, mod = divmod(len(intervals_str), 4)
-        if mod != 0:
-            raise ValueError(f'intervals_str should be a multiple of 4, got {intervals_str}')
-        intervals = []
-        for i in range(div):
-            interval_str = intervals_str[i * 4:(i + 1) * 4]
-            if interval_str == '    ':
-                interval = None
-            else:
-                interval = int(interval_str, base=12)
-            intervals.append(interval)
-        return intervals
+class Voice:
+    def __init__(self, code: str):
+        self.name, intervals_str = code.split(maxsplit=1)
+        self.intervals = [None if i == '..' else int(i, base=12) for i in intervals_str.split()]
 
 
 class Bar:
@@ -56,11 +42,12 @@ class Notation:
         self.parse(code)
     
     def parse(self, code: str):
-        header, *events = code.strip().split('\n\n')
-        self.header = Header(header)
+        events = code.strip().split('\n\n')
         self.events = [self.parse_event(event) for event in events]
         
     def parse_event(self, code: str):
+        if code.startswith('header'):
+            return Header(code)
         if code.startswith('modulation'):
             return Modulation(code)
         return Bar(code)
