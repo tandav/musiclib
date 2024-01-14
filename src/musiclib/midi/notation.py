d2c34fc0b708c29303e78dd32cb7d02840610621
Alexander Rodionov <tandav@tandav.me>
2024-01-14 14:16:01 +0300

    wip

--- src/musiclib/midi/notation.py
+++ src/musiclib/midi/notation.py
@@ -1,7 +1,11 @@
 import abc
+import collections
 from typing import NamedTuple
+import musiclib
 from musiclib.note import SpecificNote
 from musiclib.intervalset import IntervalSet
+from musiclib.midi.parse import MidiNote
+from musiclib.midi.parse import Midi
 
 
 class IntervalEvent(NamedTuple):
@@ -40,10 +44,10 @@ class Modulation(Event):
 
 class Voice:
     def __init__(self, code: str):
-        self.name, intervals_str = code.split(maxsplit=1)
-        self.intervals = self.parse_intervals(intervals_str)
+        self.channel, intervals_str = code.split(maxsplit=1)
+        self.interval_events = self.parse_interval_events(intervals_str)
         
-    def parse_intervals(self, intervals_str: str, ticks_per_beat: int = 96):
+    def parse_interval_events(self, intervals_str: str, ticks_per_beat: int = 96):
         interval = None
         on = 0
         off = 0
@@ -71,6 +75,17 @@ class Bar:
     def __init__(self, code: str):
         self.voices = [Voice(voice_code) for voice_code in code.splitlines()]
 
+    def to_midi(self, root: SpecificNote, figured_bass: bool = True):
+        channels = collections.defaultdict(list)
+        for voice in self.voices:
+            for interval_event in voice.interval_events:
+                channels[voice.channel].append(MidiNote(
+                    note=root + interval_event.interval,
+                    on=interval_event.on,
+                    off=interval_event.off,
+                ))
+        return dict(channels)
+
 
 class Notation:
     def __init__(self, code: str) -> None:
@@ -86,3 +101,40 @@ class Notation:
         if code.startswith('modulation'):
             return Modulation(code)
         return Bar(code)
+
+    def to_midi(
+        self,
+        root = SpecificNote('C', 4),
+        t_start = 0,
+        ticks_per_beat: int = 96,
+    ):
+        channels = collections.defaultdict(list)
+        t = t_start
+        for event in self.events:
+            if isinstance(event, Header):
+                root = event.root
+                ticks_per_beat = event.ticks_per_beat
+                if musiclib.__version__ != event.version:
+                    raise ValueError(f'musiclib must be exact version {event.version} to parse notation')
+            elif isinstance(event, Modulation):
+                root = event.root
+            elif isinstance(event, Bar):
+                bar_midi = event.to_midi(root)
+                
+                bar_off_channels = {channel: notes[-1].off for channel, notes in bar_midi.items()}
+                if len(set(bar_off_channels.values())) != 1:
+                    raise ValueError(f'all channels in the bar must have equal length, got {bar_off_channels}')
+                bar_off = next(iter(bar_off_channels.values()))
+                for channel, notes in bar_midi.items():
+                    for note in notes:
+                        channels[channel].append(MidiNote(note=note.note, on=t+note.on, off=t+note.off))
+                t += bar_off
+            else:
+                raise ValueError(f'unknown event type: {event}')
+
+        channels = {
+            k: Midi(notes=v, ticks_per_beat=ticks_per_beat)
+            for k, v in channels.items()
+        }
+
+        return channels
